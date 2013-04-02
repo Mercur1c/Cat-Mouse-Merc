453c2d754f12f5099523881c720a7d5ca0b1996b
Mercur1c <heptolithic@gmail.com>
2013-04-03 01:45:03 +1100

    setupt for github

--- CatMouse.py
+++ CatMouse.py
@@ -1,5 +1,5 @@
 import sys, os
-
+import math
 import pygame
 
 pygame.init()
@@ -37,7 +37,7 @@ class ControlledEntity(Entity):
 	'''An entity that is moved by a controller'''
 	def __init__(self, (x,y), controller):
 		Entity.__init__(self, (x,y))
-		self.controller = controller((x,y))
+		self.controller = controller((x,y), 1)
 		
 	def update(self):
 		self.controller.update()
@@ -47,7 +47,7 @@ class ControlledEntity(Entity):
 class Player(ControlledEntity):
 	'''The player's Mouse'''
 	def __init__(self, (x, y)):	
-		ControlledEntity.__init__(self, (x, y), MouseController)
+		ControlledEntity.__init__(self, (x, y), MouseFollower)
 		
 		self.health = 100
 		self.image = pygame.Surface((64,64))
@@ -75,16 +75,52 @@ class MouseController(Controller):
 		self.x = mousePos[0]
 		self.y = mousePos[1]
 		
-class MouseFollower(Controller):
-	'''Controller that follows the mouse'''
-	def __init__(self, (x,y), (d,t)):
+class MouseFlee(Controller):
+	'''Controller that moves away from the mouse'''
+	def __init__(self, (x,y), speed):
 		self.x = x
 		self.y = y
-		self.speed = (d,t) #Distance, Time
+		self.speed = speed
 		self.direction = 0
 		
 	def update(self):
-		pass
+		mousePos = pygame.mouse.get_pos()
+		self.direction = math.atan2(self.y-mousePos[1], self.x-mousePos[0])
+		self.heading = (math.cos(self.direction), math.sin(self.direction))
+		
+		distanceToMouse = math.hypot(mousePos[0]-self.x, mousePos[1]-self.y)
+		
+		self.step = int(self.speed)# + math.sqrt(distanceToMouse))
+		
+		self.x += self.heading[0]*self.step
+		self.y += self.heading[1]*self.step
+		
+class MouseFollower(Controller):
+	'''Controller that follows the mouse'''
+	def __init__(self, (x,y), speed):
+		self.x = x
+		self.y = y
+		self.speed = speed
+		self.direction = 0
+		
+		self.softDist = 2 #Stop the controlled sprite from jittering
+		
+	def update(self):
+		mousePos = pygame.mouse.get_pos()
+		self.direction = math.atan2(self.y-mousePos[1], self.x-mousePos[0])
+		self.heading = (math.cos(self.direction), math.sin(self.direction))
+		
+		distanceToMouse = math.hypot(mousePos[0]-self.x, mousePos[1]-self.y)
+		
+		if distanceToMouse < self.softDist:
+			pass
+		
+		else:
+			self.step = int(self.speed + math.sqrt(distanceToMouse))
+		
+			self.x -= self.heading[0]*self.step
+			self.y -= self.heading[1]*self.step
+		
 
 if __name__ == "__main__":
 	main = Main()
